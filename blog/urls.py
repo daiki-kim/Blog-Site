c6464739011176f6c5ebe29f4b83636b9d97519b
Daiki Kim <kimdaiki5021@gmail.com>
2024-03-23 13:28:10 +0900

    Create post_list and post_detail pages.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, post_list, post_detail
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("posts/", post_list, name="post_list"),
+    path("post/<int:post_id>", post_detail, name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
 def home(request):
     # Postモデルから最新の3つを取得
     posts = Post.objects.order_by("-date_posted")[:3]
-    return render(request, "blog/home.html", {"posts": posts})
+    return render(request, "home.html", {"posts": posts})
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {"posts": posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, "post_detail.html", {"post": post})
